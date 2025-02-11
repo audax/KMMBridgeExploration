d0f436b6f56f8e4b69cbb89149b1c0797a9f8e68
Jens Kadenbach <jens.kadenbach@haufe-lexware.net>
2025-02-11 09:56:45 +0100

    set package path as url to github

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
+            path: "https://github.com/audax/KMMBridgeExploration"
         )
         ,
     ]
